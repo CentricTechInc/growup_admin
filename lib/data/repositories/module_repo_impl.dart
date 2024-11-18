a2d3cf1c0ed75e131bb734da2c692dcfabb9fc00
Deepak <160821658+DeepakKumar298@users.noreply.github.com>
2024-11-18 19:40:39 +0500

    repo changes

--- lib/data/repositories/module_repo_impl.dart
+++ lib/data/repositories/module_repo_impl.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:grow_up_admin_panel/data/provider/network/apis/module_api.dart';
+import 'package:grow_up_admin_panel/data/provider/network/apis/pagination_model.dart';
 import 'package:grow_up_admin_panel/domain/entities/contribution_model.dart';
 import 'package:grow_up_admin_panel/domain/entities/gifting_model.dart';
 import 'package:grow_up_admin_panel/domain/entities/payout_model.dart';
@@ -8,29 +10,34 @@ import 'package:grow_up_admin_panel/domain/repository/module_repository.dart';
 
 class ModuleRepositoryImpl extends ModuleRepository {
   @override
-  Future<List<GiftingModel>> getGiftingTable(int pageNo) async {
+  Future<PaginationModel> getGiftingTable(int pageNo) async {
     final response = await ModuleApi.getGiftingTable(pageNo).request();
-    final List<dynamic> json = jsonDecode(response)['data'];
+    final List<dynamic> json = jsonDecode(response)['data']['records'];
     final List<GiftingModel> data =
         json.map((e) => GiftingModel.fromJson(e)).toList();
-    return data;
+    return PaginationModel(
+        data: data, count: jsonDecode(response)['data']['count']);
   }
 
   @override
-  Future<List<PayoutModel>> getPayoutTable(int pageNo) async {
+  Future<PaginationModel> getPayoutTable(int pageNo) async {
     final response = await ModuleApi.getPayoutTable(pageNo).request();
-    final List<dynamic> json = jsonDecode(response)['data'];
+    final List<dynamic> json = jsonDecode(response)['data']['data'];
     final List<PayoutModel> data =
         json.map((e) => PayoutModel.fromJson(e)).toList();
-    return data;
+    return PaginationModel(
+      data: data,
+      count: jsonDecode(response)['data']['count'],
+    );
   }
 
   @override
-  Future<List<ContributionModel>> getContributionTable(int pageNo) async {
+  Future<PaginationModel> getContributionTable(int pageNo) async {
     final response = await ModuleApi.getContributionTable(pageNo).request();
-    final List<dynamic> json = jsonDecode(response)['data'];
+    final List<dynamic> json = jsonDecode(response)['data']['data'];
     final List<ContributionModel> data =
         json.map((e) => ContributionModel.fromJson(e)).toList();
-    return data;
+    return PaginationModel(
+        data: data, count: jsonDecode(response)['data']['count']);
   }
 }
